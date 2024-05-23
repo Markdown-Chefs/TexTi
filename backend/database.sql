ca78080a42243ecfae4e421d5d14543359171c08
pyy <45996894+pang-yy@users.noreply.github.com>
2024-05-24 02:36:56 +0800

    update sql command

--- backend/database.sql
+++ backend/database.sql
@@ -2,7 +2,7 @@
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY,
   email VARCHAR(255) UNIQUE NOT NULL,
-  username TEXT NOT NULL,
+  username TEXT UNIQUE NOT NULL,
   password_hash TEXT NOT NULL,
   created_at DATE DEFAULT CURRENT_DATE
 );
