2607dc5042bda52146369b9a4bcfc99271df6dd4
pyy <45996894+pang-yy@users.noreply.github.com>
2024-06-04 12:27:00 +0800

    update validator message

--- backend/src/validators/notes.js
+++ backend/src/validators/notes.js
@@ -1,7 +1,7 @@
 const { check } = require('express-validator');
 const db = require('../config/db');
 
-const noteTitle = check('noteTitle').isLength({ min: 1, max: 255 }).withMessage("Note's title must be at least 1 characters.");
+const noteTitle = check('noteTitle').isLength({ min: 1, max: 255 }).withMessage("Note's title must be between 1 and 255 characters.");
 
 const titleExists = check('noteTitle').custom(async (value) => {
     const { rows } = await db.query('SELECT * from notes WHERE title = $1', [
