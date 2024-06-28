e4bed27bd2844c9d2952e799ffef28ba4962b270
ruiming97 <e1130094@u.nus.edu>
2024-06-28 21:49:27 +0800

    third attempt

--- backend/src/controllers/userUpdate.js
+++ backend/src/controllers/userUpdate.js
@@ -8,14 +8,15 @@ exports.changeUserUsername = async (req, res) => {
     // newUsername = req.body.newUsername
     // email = req.user.email
     try {
-        await db.query('UPDATE users SET username = $1 WHERE username = $2 AND email = $3', [
+        await db.query('UPDATE public_note_template SET username = $1 WHERE username = $2', [
             req.body.newUsername,
             req.user.username,
         ]);
 
-        await db.query('UPDATE public_note_template SET username = $1 WHERE username = $2', [
+        await db.query('UPDATE users SET username = $1 WHERE username = $2 AND email = $3', [
             req.body.newUsername,
             req.user.username,
+            req.user.email
         ]);
 
         // update user details in the session or context
