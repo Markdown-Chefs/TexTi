e50c6642b373a3a312ca52087bed0886f3f2f199
ruiming97 <e1130094@u.nus.edu>
2024-07-27 19:13:59 +0800

    added link to demo video on home page

--- frontend/src/components/appbar/appBar.jsx
+++ frontend/src/components/appbar/appBar.jsx
@@ -15,8 +15,8 @@ const AppBar = () => {
           <span className="productName">TexTi</span>
         </div>
         <div className="appbar-nav flex-row">
-          <a className="appbar-nav-link" href="#">
-              <NavLink to ='/' className="text-wrapper"> Home </NavLink> 
+          <a href="https://drive.google.com/file/d/1LaQCdkCzO5zuisAWRGwQlhmQZ2qgxoDJ/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="appbar-nav-link">
+            Demo
           </a>
           {/* <div className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
